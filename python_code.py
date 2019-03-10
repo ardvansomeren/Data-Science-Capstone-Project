b0e30192c903da064a4e2d77c62406a7f34be1a2
ardvansomeren <ard.van.someren@gmail.com>
2019-03-10 11:29:53 +0100

    added the URL for the instruction to properly set up the github repository - directly in the code with atom

--- python_code.py
+++ python_code.py
@@ -1,1 +1,2 @@
+#https://medium.com/@aklson_DS/how-to-properly-setup-your-github-repository-windows-version-ea596b398b
 print("create a python script in the local repository with VIM to push the first time")
